88a0a8d35ffb51bb4261a6d3e6d168d1f6f1c99b
Kim Kyoung-tae <kkite6789@gmail.com>
2021-01-15 17:59:28 +0900

    1/15  programmers 문자열 자르기,정렬,배열 문제 양식 바로 풀기

--- src/SubString_array.java
+++ src/SubString_array.java
@@ -4,7 +4,7 @@ public class SubString_array {
     //programmers 배열 자르고 해당번째 수 구하기
     //array의 값들에서 commands의 2차원 배열로 (첫문자열,끝문자열,몇번째숫자) 값을 정해 int배열로 return을 보낸다.
     public int[] solution(int[] array, int[][] commands) {
-
+        // 풀어보기
         return null;
     }
 }
