f09b752692013405854289c1c803ece9c8ff94f4
Kim Kyoung-tae <kkite6789@gmail.com>
2021-01-15 17:57:39 +0900

    1/15  a-b, a/b 입력받고 띄워쓰기를 이용해 출력하기

---
+++ src/SubString_array.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class SubString_array {
+    //programmers 배열 자르고 해당번째 수 구하기
+    //array의 값들에서 commands의 2차원 배열로 (첫문자열,끝문자열,몇번째숫자) 값을 정해 int배열로 return을 보낸다.
+    public int[] solution(int[] array, int[][] commands) {
+
+        return null;
+    }
+}
