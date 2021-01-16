5d1cdc0fd4ff2d4273b082e1a72e4bee4ca8ee98
Kim Kyoung-tae <kkite6789@gmail.com>
2021-01-16 23:51:53 +0900

    1/16 간단 구조 및 1/17에 전체적인 코드 수정 필수

--- src/SubString_array.java
+++ src/SubString_array.java
@@ -5,6 +5,25 @@ public class SubString_array {
     //array의 값들에서 commands의 2차원 배열로 (첫문자열,끝문자열,몇번째숫자) 값을 정해 int배열로 return을 보낸다.
     public int[] solution(int[] array, int[][] commands) {
         // 풀어보기
+
+
         return null;
+    }
+
+    public static void main(String[] args) {
+        // 메인에서 solution으로 array와 2차원배열 commands를 보낸 후
+        // solution 메소드에서 해당 값으로 처리하여 결과를 출력한다.
+        Scanner sc= new Scanner(System.in);
+        String num = sc.nextLine();
+
+        String[] array1;
+       //array1 = num.split(" ");
+       // int[] array2 = new int[array1.length];
+
+       // for(int i=0; i<array1.length; i++){
+           // array2[i] = Integer.parseInt(array1[i]);
+       // }
+    // 내일 오류 분석 및 전체적인 수정요망
+
     }
 }
