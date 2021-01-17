09896de35e688af24e32d7f6e2a8d816b5361639
Kim Kyoung-tae <kkite6789@gmail.com>
2021-01-17 23:58:53 +0900

    1/17 오류수정 및 간단한 베이스 설정

--- src/SubString_array.java
+++ src/SubString_array.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
+
 import java.util.*;
 
 public class SubString_array {
@@ -13,16 +15,20 @@ public class SubString_array {
     public static void main(String[] args) {
         // 메인에서 solution으로 array와 2차원배열 commands를 보낸 후
         // solution 메소드에서 해당 값으로 처리하여 결과를 출력한다.
+
+        System.out.println("숫자를 입력하세요 (띄워쓰기로 다음 숫자 구분)");
         Scanner sc= new Scanner(System.in);
         String num = sc.nextLine();
 
         String[] array1;
-       //array1 = num.split(" ");
-       // int[] array2 = new int[array1.length];
+        array1 = num.split(" ");
+        int[] array2 = new int[array1.length];
 
-       // for(int i=0; i<array1.length; i++){
-           // array2[i] = Integer.parseInt(array1[i]);
-       // }
+        System.out.println(array2.length);
+        for(int i=0; i<array1.length; i++){
+           array2[i] = Integer.parseInt(array1[i]);
+            System.out.println(array2[i] + " <-- array 안 "+ i + " 번째 숫자");
+        }
     // 내일 오류 분석 및 전체적인 수정요망
 
     }
