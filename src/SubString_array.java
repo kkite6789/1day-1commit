0eaffd6a59fc049ab29ee3ac84d46ba00c5cccd7
Kim Kyoung-tae <kkite6789@gmail.com>
2021-01-20 23:41:34 +0900

    1/21 인프런과 다른이유로 대체 내일 풀 문제들 정리

--- src/SubString_array.java
+++ src/SubString_array.java
@@ -7,7 +7,14 @@ public class SubString_array {
     //array의 값들에서 commands의 2차원 배열로 (첫문자열,끝문자열,몇번째숫자) 값을 정해 int배열로 return을 보낸다.
     public int[] solution(int[] array, int[][] commands) {
         // 풀어보기
-    //   1/19 모의면접준비로 대체
+    //   1/20 인프런 대체
+        /*
+            1/21 할 것들
+            문자열중에서 특정 문자열 찾기
+            반올림
+            x가 들어가 있는 문자열 정렬(정수만 적용되게)
+
+         */
 
         return null;
     }
