f5eec4aa621bec0ddb9b7b3a771e705bec638afc
Kim Kyoung-tae <kkite6789@gmail.com>
2021-01-23 23:58:58 +0900

    1/23 해당 문자열 찾기 및 횟수 검색 틀/문제

---
+++ src/SearchString.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class SearchString {
+    public static void main(String[] args) {
+        // 1. 문자열이 들어가 있는 문자
+        // 2. 찾는 문자열을 따로 지정
+        // 3. 그문자열을 찾고 몇번 들어가있는지 횟수 체크
+    }
+}
