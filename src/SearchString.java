cb5362c8cb7f43de895d594834d738800cfa5253
Kim Kyoung-tae <kkite6789@gmail.com>
2021-01-24 23:59:42 +0900

    1/24 문자열찾기 및 횟수 기본 구축

--- src/SearchString.java
+++ src/SearchString.java
@@ -5,5 +5,11 @@ public class SearchString {
         // 1. 문자열이 들어가 있는 문자
         // 2. 찾는 문자열을 따로 지정
         // 3. 그문자열을 찾고 몇번 들어가있는지 횟수 체크
+
+        String a = "123abcd452abcdef22abcd";
+        String searchText = "abcd";
+
+        int searchNum = 0;
+
     }
 }
