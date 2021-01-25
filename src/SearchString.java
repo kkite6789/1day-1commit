ad4af065a693081ea2995d5237b7380a67737a3c
Kim Kyoung-tae <kkite6789@gmail.com>
2021-01-25 15:01:08 +0900

    1/25 문자 (알파벳) 해당 문자열에서 검색 후 몇번 들어가있는지 세기

--- src/SearchString.java
+++ src/SearchString.java
@@ -1,15 +1,28 @@
+import java.sql.SQLOutput;
 import java.util.*;
-
+import java.lang.*;
 public class SearchString {
     public static void main(String[] args) {
         // 1. 문자열이 들어가 있는 문자
-        // 2. 찾는 문자열을 따로 지정
+        // 2. 찾는 문자를 따로 지정
         // 3. 그문자열을 찾고 몇번 들어가있는지 횟수 체크
 
         String a = "123abcd452abcdef22abcd";
-        String searchText = "abcd";
+        String searchText = "a";
 
-        int searchNum = 0;
+        int count = 0;
+
+        String[] b = a.split("");
+        System.out.println(b.length);
+
+        for (String s : b) {
+            if (s.equals(searchText)) {
+                count++;
+                System.out.println("loop 중");
+            }
+        }
+
+        System.out.println(searchText + " 는 " + count + "번 들어가있습니다.");
 
     }
 }
