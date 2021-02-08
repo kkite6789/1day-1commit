2d67a96de5718cd4cafe9b7a505867c1545608c7
Kim Kyoung-tae <kkite6789@gmail.com>
2021-02-08 14:21:09 +0900

    2/8 가위바위보 게임 개편 - 코드 간소화, 기타 기능 추가 1. 라운드 형태의 가위바위보게임으로 규칙 변경 2. 승점관리와 라운드 수정을 편하게 할 수 있도록 구현 3. if와 for문 축소 및 단순화 4. 컴퓨터가 매 라운드마다 같거나 다른 랜덤의 가위바위보를 낼 수 있게 변경 5. 사용자가 바로 이해할 수 있게 도움말 추가 6. 번잡한 코드를 쉽게 변경

--- src/RockScissorPaper.java
+++ src/RockScissorPaper.java
@@ -2,23 +2,27 @@ import java.util.*;
 
 public class RockScissorPaper {
     // 0-가위 1-바위 2-보
-    public static void compare(int player1, int player2){
-
-    }
-
     public static void main(String[] args) {
+        String choice = ""; // 플레이어가 고른 것
+        int computerChoice = 0; // 컴퓨터가 고른 가위,바위,보 (정수형) 0:가위 1:바위 2:보
+        int playerPick = 0; // 플레이어가 고른 가위,바위,보(정수형) 0:가위 1:바위 2:보
+        int round =3; // 라운드 횟수
+        int score=0;  // 0:패배  1:무승부  2:승리
+        int winPoint = 3; // 승리 시 점수
+        int drawPoint = 1; // 무승부 시 점수
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("가위 바위 보 중에 고르세요.");
-        String choice = ""; // 플레이어가 고른 것
-        int computerChoice = (int)(Math.random()*3)+1;
-        int playerPick = 0;
-        int turn = 1;
-        int score=0;  // 0:패배  1:무승부  2:승리
-        boolean check= true;
-        for(int i=0; i<3; i++) {
-            System.out.println(i+1 + "회차");
+        System.out.println("총 "+round+"라운드로 진행되는 가위바위보 게임");
+        System.out.println("승리 - "+winPoint+"점, 무승부 - "+drawPoint+"점 패배시 탈락");
+        System.out.println("'가위' '바위' '보' 중에 고르세요.");
+        System.out.println("3가지 외에 다른 것을 입력 시 실격입니다.");
+        System.out.println("시작합니다!");
+        System.out.println(" ");
+
+        for(int i=1; i<=3; i++) {
+            System.out.println(i + "회차");
             choice = sc.next();
+            computerChoice = (int)(Math.random()*3)+1; // 0:가위 1:바위 2:보
             if (choice.equals("가위")) {
                 playerPick = 0;
             } else if (choice.equals("바위")) {
@@ -27,49 +31,21 @@ public class RockScissorPaper {
                 playerPick = 2;
             } else {
                 System.out.println("가위 바위 보중에 입력하세요.");
-                check = false;
+                break;
             }
-        }
-        while(turn<=3){// turn 1부터 3까지
-
-            switch (playerPick) {
-                case 0 : //가위
-                    if(computerChoice == 0) {
-                        System.out.println(score + "점 획득! 비겼습니다.");
-                        score+=1;
-                    }else if(computerChoice == 1) {
-                        System.out.println(score + "점 획득.. 졌습니다.");
-                        score+=0;
-                    }else if(computerChoice == 2) {
-                        System.out.println(score + "점 획득!! 이겼습니다.");
-                        score+=2;
-                    }
-                case 1 : // 바위
-                    if(computerChoice == 0) {
-                        System.out.println(score + "점 획득!! 이겼습니다.");
-                        score+=2;
-                    }else if(computerChoice == 1) {
-                        System.out.println(score + "점 획득! 비겼습니다.");
-                        score+=1;
-                    }else if(computerChoice == 2) {
-                        System.out.println(score + "점 획득.. 졌습니다.");
-                        score+=0;
-                    }
-
-                case 2 : // 보
-                    if(computerChoice == 0) {
-                        System.out.println(score + "점 획득.. 졌습니다.");
-                        score+=0;
-                    }else if(computerChoice == 1) {
-                        System.out.println(score + "점 획득!! 이겼습니다.");
-                        score+=2;
-                    }else if(computerChoice == 2) {
-                        System.out.println(score + "점 획득! 비겼습니다.");
-                        score+=1;
-                    }
-                    turn++;
+            if((playerPick == 0 && computerChoice == 1) || (playerPick == 1 && computerChoice == 2) || (playerPick == 2 && computerChoice == 0)){
+                System.out.println(i + " 라운드 패배했습니다.");
+                System.out.println("탈락입니다.  : ( ");
+                break;
+            } else if((playerPick == 0 && computerChoice == 0) || (playerPick == 1 && computerChoice == 1) || (playerPick == 2 && computerChoice == 2)){
+                System.out.println(i + " 라운드 비겼습니다.");
+                System.out.println(winPoint + " 점 획득! 다음라운드로 진행합니다.");
+                score+=winPoint;
+            } else{
+                System.out.println(i + " 라운드 승리했습니다!");
+                System.out.println(drawPoint + " 점 획득! 다음라운드로 진행합니다.");
+                score+=drawPoint;
             }
-
         }
         System.out.println("총 점수는 : " + score + " 입니다. ");
     }
