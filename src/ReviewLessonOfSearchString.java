5443b992bd7b53a3d9359d883e89d1c97b2fed0b
Kim Kyoung-tae <kkite6789@gmail.com>
2021-02-24 17:27:56 +0900

    2/24 문자열관련 복습 기초 틀만 구현

---
+++ src/ReviewLessonOfSearchString.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ReviewLessonOfSearchString {
+    public static void main(String[] args) {
+        String str="123abcd456def";
+
+        System.out.println(str.indexOf("abc"));
+        System.out.println(str.contains("abc"));
+    }
+}
